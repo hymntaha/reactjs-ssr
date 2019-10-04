f31eafbfa74d88fa53058b9a669eccedc6658f4b
taha uygun <tahauygun@gmail.com>
2019-10-04 14:44:38 -0700

    refactoring server index.js

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -7038,15 +7038,9 @@ var _express = __webpack_require__(53);
 
 var _express2 = _interopRequireDefault(_express);
 
-var _react = __webpack_require__(15);
+var _renderer = __webpack_require__(123);
 
-var _react2 = _interopRequireDefault(_react);
-
-var _server = __webpack_require__(111);
-
-var _Home = __webpack_require__(122);
-
-var _Home2 = _interopRequireDefault(_Home);
+var _renderer2 = _interopRequireDefault(_renderer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -7054,8 +7048,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
-
-  res.send(html);
+  res.send((0, _renderer2.default)());
 });
 
 app.listen(3000, function () {
@@ -22806,6 +22799,35 @@ var Home = function Home() {
 };
 
 exports.default = Home;
+
+/***/ }),
+/* 123 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = __webpack_require__(15);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(111);
+
+var _Home = __webpack_require__(122);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
+
+    return "\n    <html>\n        <head></head>\n        <body>\n            <div id=\"root\">\n                " + content + "\n            </div>\n            <script src=\"bundle.js\"></script>\n        </body>\n    </html>\n  ";
+};
 
 /***/ })
 /******/ ]);
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import React from 'react';
-import {renderToString} from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from "./helpers/renderer";
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-
-  res.send(html);
+  res.send(renderer());
 });
 
 
