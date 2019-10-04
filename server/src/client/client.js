e2d4b4022d02b84a07722a3a19880a25f108b656
taha uygun <tahauygun@gmail.com>
2019-10-03 23:55:53 -0700

    hydrate client.js

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -4,5 +4,5 @@ import ReactDom from 'react-dom';
 import Home from './components/Home';
 
 
-ReactDom.render(<Home/>, document.querySelector('#root'));
+ReactDom.hydrate(<Home/>, document.querySelector('#root'));
 
