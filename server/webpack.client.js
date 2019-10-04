05a09b61bc5fe5f591d39d65e350bb503b26e67a
taha uygun <tahauygun@gmail.com>
2019-10-03 23:40:35 -0700

    adding client.js entry point

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,10 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  // Inform webpack that we're building a bundle
-  // for nodeJS, rather than for the browser
-  target: 'node',
-
   // Tell webpack the root file of our server application
   entry: './src/index.js',
 
