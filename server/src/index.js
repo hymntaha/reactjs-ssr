5b3c5058ae7b31f9c10cd32c115f7a66ece4e4ad
taha uygun <tahauygun@gmail.com>
2019-10-03 23:46:36 -0700

    update for index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,22 @@ import {renderToString} from 'react-dom/server';
 import Home from './client/components/Home';
 
 const app = express();
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   const content = renderToString(<Home/>);
 
+  const html = `
+    <html>
+        <head></head>
+        <body>
+            <div>
+                ${content}
+            </div>
+            <script src="bundle.js"></script>
+        </body>
+    </html>
+  `;
   res.send(content);
 });
 
