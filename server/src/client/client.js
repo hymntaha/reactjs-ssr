e9ba551b03ee70954ccca7f628b2d3bc2f9e045b
taha uygun <tahauygun@gmail.com>
2019-10-08 22:17:24 -0700

    setting up reducers

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -7,9 +7,13 @@ import {createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import {Provider} from 'react-redux';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 ReactDom.hydrate(
-  <BrowserRouter>
-    <Routes/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+  </Provider>
   , document.querySelector('#root'));
 
