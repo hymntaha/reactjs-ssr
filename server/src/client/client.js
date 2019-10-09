3c6731a22b99127bdd82bd355cc2c6eacda95405
taha uygun <tahauygun@gmail.com>
2019-10-08 22:15:23 -0700

    setup redux reducers

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import Routes from './Routes';
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {Provider} from 'react-redux';
 
 ReactDom.hydrate(
   <BrowserRouter>
