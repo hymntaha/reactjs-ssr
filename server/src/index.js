c5e0fc7a89040fce4885a2ebbe5cba26c5d5d255
taha uygun <tahauygun@gmail.com>
2019-10-08 22:22:52 -0700

    setup continues

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
+  const store = createStore();
+
+  // some logic to initialize
+  // and load data into the store
+
   res.send(renderer(req));
 });
 
