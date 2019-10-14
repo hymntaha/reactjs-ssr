97ccb125229e4e40d5be05e809e7bca2bbe163c7
taha uygun <tahauygun@gmail.com>
2019-10-13 23:25:22 -0700

    api communication

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,36 @@
-import 'babel-polyfill';
-import express from 'express';
-import {matchRoutes} from "react-router-config";
+import "babel-polyfill";
+import express from "express";
+import { matchRoutes } from "react-router-config";
 import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
+import proxy from "express-http-proxy";
 
 const app = express();
-app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqBodyDecorator(opts) {
+      opts.header["x-forwarded-host"] = "localhost:3000";
+      return opts;
+    }
+  })
+);
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
   const store = createStore();
 
-  const promises = matchRoutes(Routes, req.path).map(({route}) => {
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
   });
 
-  Promise.all(promises).then(()=> {
+  Promise.all(promises).then(() => {
     res.send(renderer(req, store));
-  })
+  });
 });
 
-
-app.listen(3000, ()=> {
-  console.log('Listening on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
