1d0510a43f784c77b472ec42b0fc827f455c4592
taha uygun <tahauygun@gmail.com>
2019-10-14 20:59:15 -0700

    client axios instance

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,22 +1,31 @@
 // Startup point for the client side application
-import 'babel-polyfill';
-import React from 'react';
-import ReactDom from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
-import Routes from './Routes';
-import {createStore, applyMiddleware} from "redux";
+import "babel-polyfill";
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import {Provider} from 'react-redux';
-import reducers from './reducers';
-import {renderRoutes} from "react-router-config";
+import axios from "axios";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import { renderRoutes } from "react-router-config";
 
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk));
+const axiosInstance = axios.create({
+  baseURL: "/api"
+});
+
+const store = createStore(
+  reducers,
+  window.INITIAL_STATE,
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
+);
 
 ReactDom.hydrate(
   <Provider store={store}>
     <BrowserRouter>
-     <div>{renderRoutes(Routes)}</div>
+      <div>{renderRoutes(Routes)}</div>
     </BrowserRouter>
-  </Provider>
-  , document.querySelector('#root'));
-
+  </Provider>,
+  document.querySelector("#root")
+);
