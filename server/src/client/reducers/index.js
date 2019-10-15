f96bf25de7b193641729c9fbf2c7bb2bd5193442
taha uygun <tahauygun@gmail.com>
2019-10-14 21:22:51 -0700

    connecting the header

--- server/src/client/reducers/index.js
+++ server/src/client/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import usersReducers from "./usersReducers";
+import authReducers from "./authReducers";
 
 export default combineReducers({
-  users: usersReducers
+  users: usersReducers,
+  auth: authReducers
 })
